0faaea044384b64f12cea766a8929cba3943faed
xxxsjan <626653354@qq.com>
2023-04-07 01:41:02 +0800

    本地搜索placeholder修改

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -7,7 +7,7 @@ var options = {
   ...flexSearchIndexOptions,
   previewLength: 100, //搜索结果预览长度
   buttonLabel: "搜索",
-  placeholder: "请输入关键词",
+  placeholder: "请输入英文关键词（本地搜索暂不支持中文）",
 };
 
 export default defineConfig({
