20d06a972347857d0c59fc88a9b7d5cce872f86b
xxxsjan <626653354@qq.com>
2023-04-07 01:38:37 +0800

    flexserach样式不用了，貌似也没有教程的失效问题

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import "./styles/vars.scss";
 import "./styles/style.scss";
-import './styles/index.css'
+// import './styles/flexsearch.css' // flexsearch插件有的版本样式丢失
 export default {
   ...DefaultTheme,
   // enhanceApp({ app }) {
