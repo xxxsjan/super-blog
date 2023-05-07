013a70280c984dc322126e0d46b3d6482af5ae17
xxxsjan <626653354@qq.com>
2023-05-07 20:56:48 +0800

    update:title

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
   base: baseMap[process.env.BUILD_ENV || process.env.NODE_ENV] || "/", // 会影响部署的读取路径
   appearance: true, // Whether to enable dark mode or not. Default: true
   lang: "zh-cn", // build时才会放到html标签里
-  title: "我的主页",
+  title: "super~blog",
   description: "记录前端学习", // 站点的描述。 这将作为<meta>标记渲染在页面HTML中。
   lastUpdated: true,
   head,
