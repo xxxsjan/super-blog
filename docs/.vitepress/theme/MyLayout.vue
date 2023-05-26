c53d0a9e9510e81e9f02c2ec186553c503c7bce9
xxxsjan <626653354@qq.com>
2023-05-26 21:40:56 +0800

    iframe显隐动画

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -2,16 +2,20 @@
   <Layout>
     <template #layout-bottom>
       <!-- 网易云外链播放器 -->
-      <iframe
-        width="330"
-        height="86"
-        frameborder="0"
-        scrolling="no"
-        allowtransparency="yes"
-        :src="src"
-        style="position: fixed; right: 0; bottom: 0; z-index: 61"
-      >
-      </iframe>
+      <div :class="{ 'iframe-container': true, hidden: !show }">
+        <div class="prefix" @click="show = !show">
+          <img src="/svg/right.svg" alt="" />
+        </div>
+        <iframe
+          width="330"
+          height="86"
+          frameborder="0"
+          scrolling="no"
+          allowtransparency="yes"
+          :src="src"
+        >
+        </iframe>
+      </div>
     </template>
   </Layout>
 </template>
@@ -19,16 +23,62 @@
 <script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
 import { useData } from "vitepress";
+import { ref } from "vue";
 const { page, theme, frontmatter } = useData();
 const { Layout } = DefaultTheme;
 const songId = "30854100";
+const show = ref(true);
 // const src = 'https://music.163.com/outchain/player?type=2&id=167876&auto=1&height=66'
 // const src =
 //   "https://music.163.com/outchain/player?type=2&id=1923325275&auto=1&height=66";
 // const src =
 //   "https://music.163.com/outchain/player?type=2&id=1340439829&auto=1&height=66";
 
-const src = `https://music.163.com/outchain/player?type=2&id=${songId}&auto=1&height=66`
+const src = `https://music.163.com/outchain/player?type=2&id=${songId}&auto=1&height=66`;
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped>
+.iframe-container {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  z-index: 61;
+
+  transition: all 1s;
+  transform: translateX(0px);
+}
+.iframe-container .prefix {
+  width: 20px;
+  height: 20px;
+  /* background-image: url("/svg/right.svg"); */
+  /* background-size: contain; */
+  /* background-repeat: no-repeat; */
+  transition: all 0.5s;
+  transform: rotate(0deg);
+}
+.prefix img {
+  animation: float 2s ease-in-out infinite;
+  object-fit: contain;
+}
+@keyframes float {
+  0% {
+    transform: translate(0, 0);
+  }
+  50% {
+    transform: translate(10px, 0px);
+  }
+  100% {
+    transform: translate(0, 0);
+  }
+}
+
+.iframe-container.hidden {
+  transform: translateX(300px);
+}
+.iframe-container.hidden .prefix {
+  transform: rotate(180deg);
+}
+</style>
