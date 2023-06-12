152a77acb64512175c03d527cc04554a142ae2fa
xxxsjan <626653354@qq.com>
2023-06-12 11:10:35 +0800

    使用组件

--- docs/.vitepress/components/Child.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>{{ count }}</div>
-</template>
-
-<script setup>
-import { inject } from "vue";
-const userData = inject("userData");
-const count = inject("count");
-</script>
--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,15 @@
 import DefaultTheme from "vitepress/theme";
 import MyLayout from "./MyLayout.vue";
+import CustomComponent from "../components/CustomComponent.vue";
+console.log("CustomComponent: ", CustomComponent);
+
 import "./styles/vars.scss";
 import "./styles/style.scss";
 
 export default {
   ...DefaultTheme,
   Layout: MyLayout,
-  // enhanceApp({ app }) {
-  //   app.use(ZIcon);
-  // },
+  enhanceApp({ app }) {
+    // app.component('CustomComponent', CustomComponent);
+  },
 };
