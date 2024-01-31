04cb7b90d2624db0d0bbb0bf4e6f20c95c9c222c
xxxsjan <626653354@qq.com>
2024-02-01 01:35:50 +0800

    音乐取消自动播放

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -40,7 +40,7 @@ const show = ref(true);
 // const src =
 //   "https://music.163.com/outchain/player?type=2&id=1340439829&auto=1&height=66";
 
-const src = `https://music.163.com/outchain/player?type=2&id=${songId}&auto=1&height=66`;
+const src = `https://music.163.com/outchain/player?type=2&id=${songId}&auto=0&height=66`;
 </script>
 
 <style scoped>
