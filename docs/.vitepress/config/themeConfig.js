94b0746f0b63300d9be0623c1afdc6fe3dff9c5d
xiesongjian <xiesongjian@dataeye.com>
2025-04-08 18:29:02 +0800

    feat(docs): add algolia search

--- docs/.vitepress/config/themeConfig.js
+++ docs/.vitepress/config/themeConfig.js
@@ -1,6 +1,6 @@
 import { sideBarObj } from "./sidebar";
 import nav from "./nav";
-import crawlerConfig from "../../../crawlerConfig.json";
+import algoliaConfig from "./algolia";
 
 // const icpRecordCode = "粤ICP备2021026613号";
 const icpRecordCode = "粤ICP备2024285819号";
@@ -10,59 +10,9 @@ const copyright = `Copyright © 2019-${new Date().getFullYear()} xxxsjan`;
 /** @type {import("vitepress").DefaultTheme.Config} */
 const themeConfig = {
   logo: "/code.png",
-
   search: {
-    //   provider: 'local',
-    provider: "algolia", // algolia搜索 https://www.algolia.com/apps/YBYUHFPZ1C/dashboard
-    options: {
-      indexName: crawlerConfig.index_name,
-      appId: "YBYUHFPZ1C",
-      apiKey: "d4076b979399477f79c55eec660730ec",
-
-      // locales: {
-      // zh: {
-      placeholder: "搜索文档",
-      translations: {
-        button: {
-          buttonText: "搜索文档",
-          buttonAriaLabel: "搜索文档",
-        },
-        modal: {
-          searchBox: {
-            resetButtonTitle: "清除查询条件",
-            resetButtonAriaLabel: "清除查询条件",
-            cancelButtonText: "取消",
-            cancelButtonAriaLabel: "取消",
-          },
-          startScreen: {
-            recentSearchesTitle: "搜索历史",
-            noRecentSearchesText: "没有搜索历史",
-            saveRecentSearchButtonTitle: "保存至搜索历史",
-            removeRecentSearchButtonTitle: "从搜索历史中移除",
-            favoriteSearchesTitle: "收藏",
-            removeFavoriteSearchButtonTitle: "从收藏中移除",
-          },
-          errorScreen: {
-            titleText: "无法获取结果",
-            helpText: "你可能需要检查你的网络连接",
-          },
-          footer: {
-            selectText: "选择",
-            navigateText: "切换",
-            closeText: "关闭",
-            searchByText: "搜索提供者",
-          },
-          noResultsScreen: {
-            noResultsText: "无法找到相关结果",
-            suggestedQueryText: "你可以尝试查询",
-            reportMissingResultsText: "你认为该查询应该有结果？",
-            reportMissingResultsLinkText: "点击反馈",
-          },
-        },
-        // },
-        // },
-      },
-    },
+    // provider: 'local',
+    ...algoliaConfig,
   },
   socialLinks: [{ icon: "github", link: "https://github.com/xxxsjan" }],
   nav,
